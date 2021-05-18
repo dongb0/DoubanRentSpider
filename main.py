efe57e2448c3f5e7d341329ec08099cd0ec6642e
dongb0 <708848999@qq.com>
2021-05-18 11:10:02 +0800

    add schedule

---
+++ main.py
@@ -0,0 +1,17 @@
+import schedule
+import time
+import os
+
+
+def job():
+    file_name = "BinjiangRent-" + time.strftime("%Y%m%d-%H%M", time.localtime()) + ".json"
+    os.system("scrapy crawl BinjiangRent -o" + file_name)
+
+
+schedule.every().day.at("22:00").do(job)
+# schedule.every(1).minutes.do(job)
+
+job()
+while True:
+    schedule.run_pending()
+    time.sleep(1)
